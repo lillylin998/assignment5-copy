8af999fe684f518fb1f0e32b4f55c293997a8721
Lilly Lin <lillylin998@users.noreply.github.com>
2020-10-27 22:55:20 -0700

    created basic buttons
    basic buttons and text input feature

--- sketch.js
+++ sketch.js
@@ -1,8 +1,63 @@
+let textInput;
+let slide;
+let b;
+let submit;
+let valText;
+let texts = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  b = createButton('change color');
+    b.position(windowWidth/2, windowHeight-100);
+  slide = createSlider(0,200);
+    slide.position(windowWidth/2, windowHeight-75);
+  textInput = createInput('');
+    textInput.position(windowWidth/2,windowHeight/2);
+   // textInput.input(textDisplay);
+  submit = createButton('submit');
+    submit.position(windowWidth/2, windowHeight/2+20);
+    submit.mousePressed(getText)
 }
 
 function draw() {
-  background(220);
-    ellipse(10,10,10);
+  background(0);
+  drawStars();
+ // submit.mousePressed(textDisplay)
+  textDisplay();
+  
+
+}
+
+function drawStars(){
+    
+}
+
+function getText(){
+    valText = textInput.value();
+    texts.push(valText);
+  //  fill(255)
+  //  text(texts[0], 10,10);
+  //  print(valText);
+}
+
+function textDisplay(){
+    if (texts[0]){
+        for(let i of texts){
+        fill(255)
+        text(i,10,10);
+       // print('hello');
+        }
+    }
+}
+
+
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
+    reposition();
+}
+
+function reposition(){
+    b.position(windowWidth/2, windowHeight-100);
+    slide.position(windowWidth/2, windowHeight-75);
+    textInput.position(windowWidth/2,windowHeight/2);
 }
